f9a9bcd8aa4c87156702f7de0139e8b9adb824b5
Emil Kristen <153062975+patters-09haulers@users.noreply.github.com>
2023-12-07 12:47:35 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// config.php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'your_username');
+define('DB_PASS', 'your_password');
+define('DB_NAME', 'your_database');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
